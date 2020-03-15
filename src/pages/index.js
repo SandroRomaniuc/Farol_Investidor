0a2b40544d39d774d399d94e033f5de467b19405
JR Dev <astronomi@gmail.com>
2020-03-15 00:23:15 -0300

    Update index.js and index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,10 @@ import Overview from './Overview';
 import Startups from './Startups';
 import Credits from './Credits';
 import Investors from './Investors';
+import Rank from './Rank';
+import Diagnostic from './Diagnostic';
+import Contacts from './Contacts';
+import Profile from './Profile';
+import Setting from './Setting';
 
-export { Overview, Startups, Credits, Investors };
+export { Overview, Startups, Credits, Investors, Rank, Diagnostic, Contacts, Profile, Setting };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import { Overview, Startups, Credits, Investors } from '../pages'
+import { Overview, Startups, Credits, Investors, Rank, Diagnostic, Contacts, Profile, Setting } from '../pages'
 // possibility passing parameters in routes
 const routes = () => (
   <Switch>
@@ -9,6 +9,11 @@ const routes = () => (
     <Route path="/startups" component={Startups} />
     <Route path="/credits" component={Credits} />
     <Route path="/investors" component={Investors} />
+    <Route path="/rank" component={Rank} />
+    <Route path="/diagnostic" component={Diagnostic} />
+    <Route path="/contacts" component={Contacts} />
+    <Route path="/profile" component={Profile} />
+    <Route path="/setting" component={Setting} />
   </Switch>
 );
 
